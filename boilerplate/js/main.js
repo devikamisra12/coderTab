ad8d72b41402664ba776a8025e9bef99ee03af87
ashikraj78 <ashikraj.78@gmail.com>
2020-06-08 12:12:36 +0530

    css for articles

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -4,86 +4,116 @@
 // You js goes here
 let articles = [
     {
-        //   title: "Understanding Positioning in CSS",
         link: "http://altcampus.io/blog/understanding-positioning-in-css/"
     },
     {
-        // title: "Basic Unix Commands",
         link: "https://altcampus.io/blog/basic-unix-commands/embed"
     },
     {
-        //   title: "Writing Semantically Correct and Clean HTML",
-        link:
-            "https://altcampus.io/blog/writing-semantically-correct-and-clean-html"
+        link:"https://altcampus.io/blog/writing-semantically-correct-and-clean-html"
     },
     {
-        //   title: "Use Console Like a Pro",
         link: "https://altcampus.io/blog/how-to-use-console-like-a-pro"
     },
     {
-        //   title: "How to build a great portfolio as a beginner developer",
-        link:
-            "https://altcampus.io/blog/how-to-build-a-great-portfolio-as-a-beginner-developer"
+        link:"https://altcampus.io/blog/how-to-build-a-great-portfolio-as-a-beginner-developer"
     },
     {
-        //   title: "Array Methods Explained With Pictorial Representation",
-        link:
-            "https://altcampus.io/blog/array-methods-explained-pictorial-representation"
+        link:"https://altcampus.io/blog/array-methods-explained-pictorial-representation"
     },
     {
-        //   title: "How to Write a Good GIT Commit Message",
         link: "https://altcampus.io/blog/how-to-write-good-git-commit-message"
     },
     {
-        //   title: "Making VSCode Extension",
         link: "https://www.freecodecamp.org/news/making-vscode-extension"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
-        link:
-            "https://www.freecodecamp.org/news/implementing-a-linked-list-in-javascript"
+        link:"https://www.freecodecamp.org/news/implementing-a-linked-list-in-javascript"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://altcampus.io/blog/quiz/git-commands-1"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://altcampus.io/blog/quiz/html-elements"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "http://perfectionkills.com/javascript-quiz"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://www.sanfoundry.com/javascript-mcqs-lexical-structure"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "http://davidshariff.com/js-quiz/"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://www.wscubetech.com/quiz-test-javascript"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://dev.to/lydiahallie/interactive-javascript-quiz-1-1flc"
     },
     {
-        //   title: "Implemnting a Linked List in JavaScript",
         link: "https://dev.to/lydiahallie/interactive-javascript-quiz-2-4pi1"
+    },
+    {
+        link: "https://medium.freecodecamp.org/how-to-think-like-a-programmer-lessons-in-problem-solving-d1d8bf1de7d2"
+    },
+    {
+        link: "https://medium.com/zerotomastery/dont-be-a-junior-developer-608c255b3056"
+
+    },
+    {
+        link: "https://medium.freecodecamp.org/the-main-pillars-of-learning-programming-and-why-beginners-should-master-them-e04245c17c56"
+    },
+    {
+        link: "https://medium.com/@benbob/what-i-learned-from-working-for-both-bill-gates-and-steve-jobs-f0c04e1e5160"
+    },
+    {
+        link: "https://engineering.videoblocks.com/web-architecture-101-a3224e126947"
+    },
+    {
+        link: "https://edgecoders.com/the-mistakes-i-made-as-a-beginner-programmer-ac8b3e54c312"
+    },
+    {
+        link: "http://www.akkadia.org/drepper/cpumemory.pdf"
+    },
+    {
+        link: "https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html"
+    },
+    {
+        link: "http://blog.lunatech.com/2009/02/03/what-every-web-developer-must-know-about-url-encoding"
+    },
+    {
+        link: "https://programmers.stackexchange.com/questions/46716/what-technical-details-should-a-programmer-of-a-web-application-consider-before"
+    },
+    {
+        link: "http://katemats.com/what-every-programmer-should-know-about-seo/"
+    },
+    {
+        link: "http://blog.llvm.org/2011/05/what-every-c-programmer-should-know.html"
+    },
+    {
+        link: "http://gafferongames.com/networking-for-game-programmers/what-every-programmer-needs-to-know-about-game-networking/"
+    },
+    {
+        link: "https://stackoverflow.com/questions/2794016/what-should-every-programmer-know-about-security"
+    },
+    {
+        link: "https://opensource.com/article/18/11/multiple-programming-languages"
+    },
+    {
+        link: "https://opensource.com/article/18/9/how-many-programming-languages"
+    },
+    {
+        link: "https://opensource.com/article/18/10/next-programming-language-you-want-learn"
     }
 ];
+
 
 let article = document.querySelector('.article');
 
